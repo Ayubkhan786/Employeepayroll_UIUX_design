6e5c2cdd12fd6b531f496c7260220c060b26408b
your Name <mohamedabdulayubm@gmail.com>
2022-08-11 15:20:37 +0530

    [Ayub] Modified using getter and setter

---
+++ EmployeePayroll/pages/EmployeePayroll.js
@@ -0,0 +1,77 @@
+class EmployeeePayrollData {
+  // Name;
+  // ProfileImage
+  // Gender;
+  // Department;
+  // Salary;
+  // StartDate;
+
+  constructor(...params) {
+    this.name = params[0];
+    this.ProfileImage = params[1];
+    this.gender = params[2];
+    this.Department = params[3];
+    this.salary = params[4];
+    this.startDate = params[5];
+  }
+  get Department() {
+    return this._Department;
+  }
+  set Department(Department) {
+    this._Department = Department;
+  }
+  get ProfileImage() {
+    return this._ProfileImage;
+  }
+  set ProfileImage(ProfileImage) {
+    this._ProfileImage = ProfileImage;
+  }
+  get gender() {
+    return this._gender;
+  }
+  set gender(gender) {
+    this._gender = gender;
+  }
+  get salary() {
+    return this._salary;
+  }
+  set salary(salary) {
+    this._salary = salary;
+  }
+  get startDate() {
+    return this._startDate;
+  }
+  set startDate(startDate) {
+    this._startDate = startDate;
+  }
+  get name() {
+    return this._name;
+  }
+  set name(name) {
+    let nameRegex = RegExp("^[A-Z]{1}[a-z]{3,}$");
+    if (nameRegex.test(name)) this._name = name;
+    else throw "Name is incorrect";
+  }
+
+  toString() {
+    const option = { year: "numeric", month: "long", day: "numeric" };
+    const empDate =
+      this.startDate === undefined
+        ? "undefined"
+        : this._startDate.toLocaleDateString("en-US", option);
+    return (
+      ", Department= " +
+      this.Department +
+      ", name = " +
+      this.name +
+      ", ProfileImage" +
+      this.ProfileImage +
+      ", salary = " +
+      this.salary +
+      ", Gender = " +
+      this.gender +
+      " , StartDate = " +
+      empDate
+    );
+  }
+}
